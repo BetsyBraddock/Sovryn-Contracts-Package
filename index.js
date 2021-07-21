48b18f632d83174e92c50eb0877cc675e2e6016b
BetsyBraddock <k.shall0507+mutant@gmail.com>
2021-07-21 15:26:37 +0100

    First version of sovryn contracts package

---
+++ index.js
@@ -0,0 +1,8 @@
+import contractsMainnet from "./contracts-mainnet.json"
+import contractsTestnet from "./contracts-testnet.json"
+import * as abis from "./abi"
+
+exports.contractsMainnet = contractsMainnet
+exports.contractsTestnet = contractsTestnet
+exports.abis = abis
+
