d856366fc4c6cba918f3ea4ca7e3b11717ab2695
BetsyBraddock <k.shall0507+mutant@gmail.com>
2021-08-30 11:21:34 +0100

    Changing branches

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,7 @@ import contractsTestnet from './contracts-testnet.json';
 import { contractMap } from './build-contracts';
 import { AddressListData } from './build-contracts';
 
-console.log(`Using ${process.env.NETWORK_MODE} mode`);
-
-export const addresses =
-  process.env.NETWORK_MODE && process.env.NETWORK_MODE === 'mainnet'
-    ? (contractsMainnet as AddressListData)
-    : (contractsTestnet as AddressListData);
+export const mainnetAddresses: AddressListData = contractsMainnet;
+export const testnetAddresses: AddressListData = contractsTestnet;
 
 export const contracts = contractMap;
