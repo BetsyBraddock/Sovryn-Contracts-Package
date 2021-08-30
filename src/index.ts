7bdd79761bf38ae1c716fec0fb473b30d3937275
BetsyBraddock <k.shall0507+mutant@gmail.com>
2021-08-30 11:33:13 +0100

    Simpler package that just exports all addresses and abis

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import contractsMainnet from './contracts-mainnet.json';
 import contractsTestnet from './contracts-testnet.json';
-import { contractMap } from './build-contracts';
-import { AddressListData } from './build-contracts';
+import complete from './abi/abiComplete.json';
+import erc20 from './abi/abiEERC20.json';
+import liquidityMining from './abi/mining_proxy.json';
+import { AbiItem } from 'web3-utils';
+
+type AddressListData = {
+  [key: string]: string;
+};
 
 export const mainnetAddresses: AddressListData = contractsMainnet;
 export const testnetAddresses: AddressListData = contractsTestnet;
 
-export const contracts = contractMap;
+export const abiComplete = complete as AbiItem[];
+export const abiErc20 = erc20 as AbiItem[];
+export const abiLiquidityMining = liquidityMining as AbiItem[];
