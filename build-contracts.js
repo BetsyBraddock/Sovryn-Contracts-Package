87cb84bf7ca9ba1e45c2b9dc96988b376d7e1289
unknown <k.shall0507+mutant@gmail.com>
2022-01-06 15:48:23 +0000

    Added abiInterestUser to testnet protocol contract

--- build-contracts.js
+++ build-contracts.js
@@ -140,5 +140,5 @@ if (process.env.NETWORK_MODE && process.env.NETWORK_MODE === "mainnet") {
     module.exports.AdoptionFund = new web3.eth.Contract(abiDevelopmentFund, addresses.AdoptionFund.toLowerCase());
     module.exports.Development_Fund = new web3.eth.Contract(abiDevelopmentFund, addresses.DevelopmentFund.toLowerCase());
 } else {
-    module.exports.sovrynProtocol = new web3.eth.Contract(abiCompleteNew.concat(abiSwapsExternal).concat(abiAffiliateFees).concat(abiProtocolSettings), addresses.Protocol.toLowerCase())
+    module.exports.sovrynProtocol = new web3.eth.Contract(abiCompleteNew.concat(abiSwapsExternal).concat(abiAffiliateFees).concat(abiProtocolSettings).concat(abiInterestUser), addresses.Protocol.toLowerCase())
 }
